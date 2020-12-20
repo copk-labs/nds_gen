48bec1ba0f4ccc1700d2ee38e3cb90ff2e4ea15b
Charles Leroy <leroycharles@vivaldi.net>
2020-12-20 13:05:11 +0100

    feat: #2 - Greetings
    Implémentation et test de la fonction Greetings sous Windows.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
-﻿namespace NDS_GEN
+﻿using System;
+
+namespace NDS_GEN
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            Greetings();
+            return 0;
+        }
+
+        private static void Greetings()
+        {
+            Console.WriteLine("NDS GEN V-202012");
+            Console.WriteLine("Générateur de Notes de Service pour COPK LABS");
         }
     }
 }
